0acf2437d3b77b30155a135216aaa94f8badc010
IhsanKirson <ucupcampina66@gmail.com>
2020-03-10 15:34:05 +0700

    Updates

---
+++ precache-manifest.a823f37578115eba29d81175118ecb82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df887e86ba4916df9af815fa74e462bc",
+    "url": "/belajar_reactjs/index.html"
+  },
+  {
+    "revision": "21059e424fc5924cec34",
+    "url": "/belajar_reactjs/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0f72b90fb84a1b6ae63",
+    "url": "/belajar_reactjs/static/js/2.5b4d22bd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/belajar_reactjs/static/js/2.5b4d22bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21059e424fc5924cec34",
+    "url": "/belajar_reactjs/static/js/main.9565cb0b.chunk.js"
+  },
+  {
+    "revision": "b980a4daf196c5e2d01a",
+    "url": "/belajar_reactjs/static/js/runtime-main.f0e0f27c.js"
+  }
+]);
